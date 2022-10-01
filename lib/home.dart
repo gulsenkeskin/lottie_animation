e693af70b38d331ca1106723762041ca87cb86ed
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-01 13:00:49 +0300

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -11,12 +11,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: const Text('Lottie Animations'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Lottie.network('https://assets9.lottiefiles.com/packages/lf20_jmejybvu.json'),
-    )
-  );
+      appBar: AppBar(
+        title: const Text('Lottie Animations'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Lottie.asset('assets/delivery.json'),
+          //Lottie.network('https://assets9.lottiefiles.com/packages/lf20_jmejybvu.json'),
+          const SizedBox(height: 32),
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.delivery_dining,
+              size: 40,
+            ),
+            label:const Text('Order Pizza'),
+            style: ElevatedButton.styleFrom(
+              padding:const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              textStyle:const TextStyle(fontSize: 28),
+            ),
+          )
+        ],
+      ));
+
+
+
 }
